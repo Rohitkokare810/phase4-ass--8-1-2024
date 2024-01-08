429a7aeeeb9d1e499da9344af3584b2b6a5d8140
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-08 16:50:04 +0530

    initial commit

---
+++ ClassLibrary1/RegLogin/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegLogin
+{
+    public class UserLoginReg
+    {
+        public string Login(string username, string password)
+        {
+            string result;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                result = "please provide username and password";
+            }
+            else
+            {
+                if (username == "sam" && password == "sam@123")
+                {
+                    result = "Login Pass";
+                }
+                else
+                {
+                    result = "Login Fail";
+                }
+            }
+            return result;
+        }
+
+        public string Register(string username, string password)
+        {
+            string result;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                result = "please provide username and password";
+            }
+            else
+            {
+
+
+                result = "Registered successfully";
+
+            }
+            return result;
+        }
+
+
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ClassLibrary1/RegLogin/SampleTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegLogin
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        UserLoginReg loginReg;
+        [SetUp]
+
+        public void SetUp()
+        {
+            loginReg = new UserLoginReg();
+        }
+        [Test]
+        public void LogTest()
+        {
+            string name, password,res;
+            name = "sam";
+            password = "sam@123";
+            res = "Login Pass";
+            Assert.AreSame(res, loginReg.Login(name, password));
+        }
+
+        [Test]
+        public void LogTest2()
+        {
+            string name, password, res;
+            name = "rohit";
+            password = "rohit@123";
+            res = "Login Fail";
+            Assert.AreSame(res, loginReg.Login(name, password));
+        }
+
+        [Test]
+        public void RegTest()
+        {
+            string name, password, res;
+            name = "rohit";
+            password = "rohit@123";
+            res = "Registered successfully";
+            Assert.AreSame(res, loginReg.Register(name, password));
+        }
+
+        [Test]
+        public void RegTest2()
+        {
+            string name, password, res;
+            name = "";
+            password = "";
+            res = "please provide username and password";
+            Assert.AreSame(res, loginReg.Register(name, password));
+        }
+
+
+
+    }
+}
